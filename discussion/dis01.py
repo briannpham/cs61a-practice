9e79a80887b9807e4d4fc74e40e1a49304f1fffb
brpham13 <br.pham13@gmail.com>
2022-11-25 22:13:24 -0800

    add dis01 - dis03

---
+++ discussion/dis01.py
@@ -0,0 +1,62 @@
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    if n < 2:
+        return False
+    k = 2
+    while k < n:
+        if n % k == 0:
+            return False
+        k = k + 1
+    return True
+
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    unique = 0
+    while n > 0:
+        n, last = n // 10, n % 10
+        if not has_digit(n, last):
+            unique += 1
+    return unique
+
+def has_digit(n, k):
+    """Returns whether K is a digit in N.
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    while n > 0:
+        n, last = n // 10, n % 10
+        if last == k:
+            return True
+    return False
+
+def unique_digits_alt(n):
+    unique = 0
+    i = 0
+    while i < 10:
+        if has_digit(n, i):
+            unique += 1
+        i += 1
+    return unique
