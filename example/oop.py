6aec551d59acd5d56bf4f761c70bd04a7629f67a
Brian Pham <br.pham13@gmail.com>
2022-12-02 12:23:57 -0800

    finish oop and inheritance

--- example/oop.py
+++ example/oop.py
@@ -12,3 +12,49 @@ class Account:
             return 'Insufficient funds'
         self.balance -= amount
         return self.balance
+
+class CheckingAccount(Account):
+    interest = 0.01
+    withdraw_fee = 1
+    def withdraw(self, amount):
+        return Account.withdraw(self, amount + self.withdraw_fee)
+
+class SavingAccount(Account):
+    deposit_fee = 2
+    def deposit(self, amount):
+        return Account.deposit(self, amount - self.deposit_fee)
+
+class AsSeenOnTVAccount(CheckingAccount, SavingAccount):
+    def __init__(self, account_holder):
+        self.holder = account_holder
+        self.balance = 1
+
+class Bank:
+    """A bank *has* accounts.
+
+    >>> bank = Bank()
+    >>> john = bank.open_account('John', 10)
+    >>> jack = bank.open_account('Jack', 5, CheckingAccount)
+    >>> john.interest
+    0.02
+    >>> jack.interest
+    0.01
+    >>> bank.pay_interest()
+    >>> john.balance
+    10.2
+    """
+    def __init__(self):
+        self.accounts = []
+
+    def open_account(self, holder, amount, kind=Account):
+        account = kind(holder)
+        account.deposit(amount)
+        self.accounts.append(account)
+        return account
+    
+    def pay_interest(self):
+        for a in self.accounts:
+            a.deposit(a.balance * a.interest)
+
+    def too_big_to_fail(self):
+        return len(self.accounts) > 1
